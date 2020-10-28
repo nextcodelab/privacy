facdf92f00d68b360d5604578964ecbfac624a66
nextcodelab <73583588+nextcodelab@users.noreply.github.com>
2020-10-28 18:44:22 +0800

    f
    first

---
+++ All.js
@@ -0,0 +1,39 @@
+var downloadTxtUrl = 'https://raw.githubusercontent.com/nextcodelab/HostJson/main/DeveloperInfo.json';
+var responseResult;
+function ChangeText() {
+
+    var AppName = responseResult.Application;
+    var Developer = responseResult.Developer;
+    var ContactMail = responseResult.Email;
+    var theDate = responseResult.Date;
+    var p_tag = "p_tag";
+    for (i = 0; i <= 15; i++) {
+        var tag_id = document.getElementById(p_tag + i);
+        tag_id.innerHTML = tag_id.innerHTML.replace(/ApplicationName/g, AppName).replace(/DeveloperName/g, Developer);
+    }
+    var contactTag = document.getElementById("tag_contact_email");
+    contactTag.innerHTML = contactTag.innerHTML.replace(/ContactEmail/g, ContactMail);
+
+    var date_Tag = document.getElementById("tag_date");
+    date_Tag.innerHTML = date_Tag.innerHTML.replace(/TheDate/g, theDate);
+    
+}
+window.onload = loadAll;
+
+function loadAll() {
+   getJson();
+}
+function getJson() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', downloadTxtUrl, 'jsonp');
+    xhr.responseType = 'json';
+    xhr.onload = function (e) {
+        if (this.status == 200) {
+            console.log('response', this.response); // JSON response 
+            responseResult = this.response;
+            ChangeText();
+        }
+    };
+    xhr.send();
+}
+
